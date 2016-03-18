e4a2f55f2047ac950a902adb5f385d285ed71d6c
Bill <qxu@coremail.cn>
2016-03-18 17:15:35 +0800

    Initial Commit

---
+++ XQConstraintDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XQConstraintDemo
+//
+//  Created by Lunkr on 16/3/18.
+//  Copyright © 2016年 Lunkr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XQConstraintDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XQConstraintDemo
+//
+//  Created by Lunkr on 16/3/18.
+//  Copyright © 2016年 Lunkr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
